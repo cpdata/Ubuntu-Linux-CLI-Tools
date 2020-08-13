4959fc60f67ed2fd35cac46dc5b12391fe0f3705
Charles P. Cross <charlie@cpdatadesigns.com>
2020-08-12 21:51:23 -0400

    Update makewebsite.sh
    Should work the same

--- makewebsite.sh
+++ makewebsite.sh
@@ -4,45 +4,46 @@
 # FILE   : makewebsite.sh
 # AUTHOR : Charles Paul Cross - charlie@cpdatadesigns.com
 #
-# DESCRIPTION : Guides user with terminal prompt questions to configure and setup a new website on an 
-# 		linux/apache2 webserver. This is Useful for shared hosting. Apache Root must be set to /var/www 
-#		else this file needs to be modified to where apache2 has root access. 
-#		The root directory of the new site will be /var/www/SITENAMEHERE/public_html
+# DESCRIPTION : Guides user with terminal prompt questions to configure and setup a new website on an
+#                               linux/apache2 webserver. This is Useful for shared hosting. Apache Root must be set to /var/www else
+#                               this file needs to be modified to where apache2 has route access.
+#                               The root directory of the new site will be /var/www/SITENAMEHERE/public_html
 #
 # 1)Creates proper directory tree for a new website using the new user inputed domain name
 # 2)If provided zip file will be extracted to the proper folder to populate the site
 # 3)If SSL is needed a temporary self signed certificate is created along with a CSR that can be used to get real SSL
 #   If SSL is selected as yes then the CSR and self signed are set up. Always select yes unless the site will never
-#	use SSL which is not recommended.
+#       use SSL which is not recommended.
 # 4)Apache2 .conf file is created differently if ssl was selected then the site.conf is enabled for the provided domain
 # 5)Sets proper certificate permissions and directory ownership
 # 6)Reloads Apache2
 #
-# NOTES: 
+# NOTES:
 # 1)Assumes site directorys should be created as /var/www/SITENAMEHERE/public_html
 # 2)Currently only unzips .zip files . tar.gz coming soon. You don't need a zip file. leave it blank and it will just
-#   create a default welcome index.php page you can replace once you upload files to the directory. It's there to let 
-#	you know your DNS is pointing correctly and you did everything right. Or if its' a new site or you don't have a
-#	zip file.
+#   create a default welcome index.php page you can replace once you upload files to the directory. It's there to let
+#       you know your DNS is pointing correctly and you did everything right. Or if it is a new site or you don't have a#       zip file.
 # 3)SSL support creates Self-Signed certificate 50 years
 # 4)Always run with sudo
-# 5)The First 3 key prompts for creating the SSL is just for you it can be as simple as 1234 you just have to remember 
-#   it until the final step of the prompts. When asked for the Challenge password it is not the same thing as the key it #	asked for in the beggining. You can 	 		leave this blank if not you may need to provide it to your SSL
-#	certificate provider.
+# 5)The First 3 key prompts for creating the SSL is just for you it can be as simple as 1234 you just have to remember
+#   it until the final step of the prompts. When asked for the Challenge password it is not the same thing as the key it
+#       asked for in the beggining. You can leave this blank if not you may need to provide it to your SSL
+#       certificate provider.
 #
 # Extended Commands: ./makewebsite.sh {DOMAIN_NAME} {FULL_PATH_TO_ZIP_FILE} {BOOL IS_USING_SSL}
 # Example Use: sudo ./makewebsite.sh domain.com domain.com.zip 1
 #
-# LICENCE: 
+# COPYRIGHT: Any reproduction or use of this file is forbidden unless permission is given by the author.
 #
-#
-
+#####
+#Sudo has been added to every command
+#####
 # Make sure only root can run our script
 #if [[ $EUID -ne 0 ]]; then
-#   echo "This script must be run with sudo or as root ex.(sudo ./makewebsite.sh)" 1>&2   
+#   echo "This script must be run with sudo or as root ex.(sudo ./makewebsite.sh)" 1>&2
 #   exit 1
 #fi
-
+######
 
 NEWSITE=$1
 SITEFILES=$2
@@ -133,7 +134,7 @@ sudo echo "<VirtualHost *:80>
         SSLEngine on
         SSLCertificateFile /var/www/$SITENAME/ssl/certs/$SITENAME.crt
         SSLCertificateKeyFile /var/www/$SITENAME/ssl/keys/$SITENAME.key
-	#SSLCertificateChainFile /var/www/$SITENAME/ssl/certs/$SITENAME.ca-bundle
+        #SSLCertificateChainFile /var/www/$SITENAME/ssl/certs/$SITENAME.ca-bundle
 </VirtualHost>
 
 </IfModule>" > $SITENAME.conf
@@ -239,4 +240,3 @@ sudo chmod -R 600 /var/www/$NEWSITE/ssl/keys
 sudo chmod -R 600 /var/www/$NEWSITE/ssl/csrs
 
 echo "--- !!!FINISHED!!! --- $NEWSITE setup complete!"
-
